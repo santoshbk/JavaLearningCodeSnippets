1c4f13611fb108fc781a86f54465406e778669c9
Santosh B K <santosh.b.k@hp.com>
2015-06-02 16:35:09 +0530

    Roy Life cycle

--- JavaCodeSnippets/src/com/problems/roy/lifecycle/RoysLifeCycle.java
+++ JavaCodeSnippets/src/com/problems/roy/lifecycle/RoysLifeCycle.java
@@ -2,8 +2,6 @@ package com.problems.roy.lifecycle;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RoysLifeCycle {
 
@@ -14,27 +12,57 @@ public class RoysLifeCycle {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
 		int N = Integer.parseInt(line);
-		 String[] days = new String[N];
+		String[] days = new String[N];
 		for (int i = 0; i < N; i++) {
 			days[i] = br.readLine();
 		}
-		int[] counts = new int[N];
-		Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
-		int i=0;
+		// int[] counts = new int[N];
+		// Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+		int i = 0;
 		int largeDay = 0;
+		int maxStreak = 0;
+		int streak = 0;
+		boolean flag = false;
 		for (String day : days) {
 			int count = 0;
 			for (char c : day.toCharArray()) {
-				if(c=='C' || c=='c')
+				if (c == 'C' || c == 'c') {
 					count++;
+					streak++;
+				} else {
+					flag = true;
+				}
+				if (flag) {
+					if (count != 0)
+						largeDay = largeDay(largeDay, count);
+					count = 0;
+					flag = false;
+					maxStreak = largeDay(maxStreak, streak);
+					streak = 0;
+				}
 			}
-			counts[i++]=count;
-			
+			largeDay = largeDay(largeDay, count);
+			if (i == 0) {
+				maxStreak = streak;
+			}
+			if (flag) {
+				maxStreak = largeDay(maxStreak, streak);
+				streak = 0;
+			}
+			i++;
+			largeDay = largeDay(largeDay, count);
+
 		}
-		
-		
-		
+		System.out.println(largeDay + " " + maxStreak);
+
 		/* */
+	}
+
+	private static int largeDay(int largeDay, int count) {
+		if (largeDay < count) {
+			largeDay = count;
+		}
+		return largeDay;
 	}
 
 }
